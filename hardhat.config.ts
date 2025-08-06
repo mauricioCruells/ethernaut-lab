ee41b91e315e23464c8170f3ca62634816275b07
m.cruells <mcruells.dev@gmail.com>
2025-08-06 14:08:12 -0600

    feat(hardhat): add config to hardhat.config.ts
    Add network config for sepolia and hardhat networks
    
    Setup  hardhat as default network
    
    Add paths for future reference

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,22 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/YOUR_INFURA_PROJECT_ID",
+      accounts: []
+    }
+  },
   solidity: "0.8.28",
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
 };
 
 export default config;
