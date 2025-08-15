9881e58870fe524e85aa465174d0069c528d5b9c
mcruells <mcruells.dev@gmail.com>
2025-08-15 19:51:56 +0000

    hotfix(config): comment sepolia config for testing

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,9 @@
 import { HardhatUserConfig, task, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
-const INFURA_API_KEY = vars.get("INFURA_API_KEY");
-const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+// const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+// const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+// const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 
 
 const config: HardhatUserConfig = {
@@ -12,10 +12,10 @@ const config: HardhatUserConfig = {
     hardhat: {
       chainId: 1337
     },
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY]
-    }
+    // sepolia: {
+    //   url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+    //   accounts: [SEPOLIA_PRIVATE_KEY]
+    // }
   },
   solidity: "0.8.28",
   paths: {
@@ -24,10 +24,10 @@ const config: HardhatUserConfig = {
     cache: "./cache",
     artifacts: "./artifacts"
   },
-  etherscan: {    
-    apiKey: {
-      sepolia: ETHERSCAN_API_KEY,
-    }
+  // etherscan: {    
+  //   apiKey: {
+  //     sepolia: ETHERSCAN_API_KEY,
+  //   }
   },
 };
 
